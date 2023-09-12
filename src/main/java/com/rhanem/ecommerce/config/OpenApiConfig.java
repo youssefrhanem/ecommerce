3ef44e5e9f207d439dccbe959a0efb1a0fbb654e
youssef rhanem <youssefrhanem91@gmail.com>
2023-09-12 21:58:44 +0100

    Initial commit with an api swagger config and h2 db

---
+++ src/main/java/com/rhanem/ecommerce/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.rhanem.ecommerce.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+
+
+    @Bean
+    public OpenAPI userServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Ecommerce API")
+                        .description("Ecommerce Web Application API")
+                        .version("1.0"));
+    }
+}
---
+++ src/main/java/com/rhanem/ecommerce/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.rhanem.ecommerce.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+
+    @GetMapping("/status")
+    public String statusCheck() {
+        return "working...";
+    }
+}
